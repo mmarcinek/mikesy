d53b9a0a4656b332613db909763013972b738a79
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-21 14:02:49 -0400

    jQuery written, data does not append. Needs debug.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 $(document).ready( function () {
 
-var listing = $(#item);
-var itemImage, itemTitle, itemSeller, itemPrice;
+var itemImage, itemTitle, itemSeller, itemPrice, itemPost;
 
 cameras.forEach( function (item){
+    itemImage = '<img src="' +item.url_570xn + '">';
+    itemTitle = '<p>' + item.title + '</p>';
+    itemSeller ='<p>' + item.shop_name + '</p>';
+    itemPrice ='<p>' + '$' + item.Price + ' ' + 'USD';
+    itemPost = '<li>' + itemImage + itemTitle + itemSeller + itemPrice + '</li>';
 
+$('#listing').append(item);
 
 })
 
