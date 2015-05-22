4a8fa8d18d81fa2a3e4d0952139e8ab919456cd9
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-22 13:48:19 -0400

    Sidebar links are populating, sort(); provides no error and returns an array, but still needs modification to work properly.
    Return to main.js for final fix.

--- scripts/main.js
+++ scripts/main.js
@@ -18,10 +18,25 @@ $('.listing').append(itemPost);
 var categoryListing;
 
 stuff.results.forEach ( function (list){
-    categoryListing = '<li><a href="#">' + list.taxonomy_path + '</a></li>';
 
-$('.sidebar').append(categoryListing).sort();
+categoryListing = '<li><a href="#">' + list.taxonomy_path + '</a></li>';
+
+// $('.sidebar').append(categoryListing);
+
+$('.sidebar').sort(function (categoryListing) {
+   if(a.categoryListing == b.categoryListing){
+    return a.categoryListing;} else {
+    return a.categoryListing && b.categoryListing;}
+   }).append(categoryListing);
+
 
 });
 
 });
+
+// sort( function (x,y) {
+//    var x = results[0].taxonomy_path[0];
+//    var y = results[1].taxonomy_path[1];
+//    if(x === y) {
+//     return x;} else {
+//     return x && y;} });
